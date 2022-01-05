e0794332d42fa0c703ce1e386df3b9fb1119e305
Pranav Jain <42434949+Pranavjain23@users.noreply.github.com>
2022-01-05 19:33:19 +0530

    Update CNN.py

--- CNN.py
+++ CNN.py
@@ -5,60 +5,61 @@
 #CNN to detect plant diseases in Maize plant leaves
 #Train accuracy ~ 96.2% and Test accuracy ~ 93%
 
-import keras
-from keras.models import Sequential
-from keras.layers import Conv2D,MaxPooling2D,Flatten,Dropout,Dense,BatchNormalization,SpatialDropout2D
+import tensorflow
+from tensorflow.keras.layers import Conv2D, Flatten, Dense, Dropout,MaxPooling2D,Flatten, BatchNormalization,SpatialDropout2D
+from tensorflow.keras.models import Model,Sequential
 
 model=Sequential()
 
-model.add(Conv2D(32,(3,3),padding='same',activation='relu',input_shape=(128,128,3)))
+model.add(Conv2D(32,(3,3),padding='same',activation='relu',input_shape=(150,150,3)))
 model.add(MaxPooling2D(2,2))
-model.add(SpatialDropout2D(0.1))
+#model.add(SpatialDropout2D(0.1))
 
 model.add(Conv2D(32,(3,3),padding='same',activation='relu'))
 model.add(MaxPooling2D(2,2))
 model.add(BatchNormalization())
-model.add(SpatialDropout2D(0.2))
+#model.add(SpatialDropout2D(0.2))
 
 model.add(Conv2D(32,(3,3),padding='same',activation='relu'))
 model.add(MaxPooling2D(2,2))
 model.add(BatchNormalization())
-model.add(SpatialDropout2D(0.2))
+#model.add(SpatialDropout2D(0.2))
 
 model.add(Conv2D(32,(3,3),padding='same',activation='relu'))
 model.add(MaxPooling2D(2,2))
 model.add(BatchNormalization())
-model.add(SpatialDropout2D(0.3))
+#model.add(SpatialDropout2D(0.3))
 
 model.add(Conv2D(32,(3,3),padding='same',activation='relu'))
 model.add(MaxPooling2D(2,2))
 model.add(BatchNormalization())
-model.add(SpatialDropout2D(0.3))
+#model.add(SpatialDropout2D(0.3))
 
 model.add(Conv2D(32,(3,3),padding='same',activation='relu'))
 model.add(MaxPooling2D(2,2))
 model.add(BatchNormalization())
-model.add(SpatialDropout2D(0.3))
+#model.add(SpatialDropout2D(0.3))
 
 model.add(Conv2D(32,(3,3),padding='same',activation='relu'))
 model.add(MaxPooling2D(2,2))
 model.add(BatchNormalization())
-model.add(SpatialDropout2D(0.5))
+#model.add(SpatialDropout2D(0.5))
 
 model.add(Flatten())
-model.add(Dropout(0.5))
-model.add(Dense(output_dim=128,activation='relu'))
-model.add(Dropout(0.5))
-model.add(Dense(output_dim=128,activation='relu'))
-model.add(Dropout(0.5))
-model.add(Dense(output_dim=1,activation='sigmoid'))
-
-adam=keras.optimizers.Adam(learning_rate=0.001)
-model.compile(optimizer=adam,loss='binary_crossentropy',metrics=['accuracy'])
+#model.add(Dropout(0.5))
+model.add(Dense(128,activation='relu'))
+#model.add(Dropout(0.5))
+model.add(Dense(128,activation='relu'))
+model.add(Dense(32,activation='relu'))
+#model.add(Dropout(0.5))
+model.add(Dense(4,activation='softmax'))
 
 model.summary()
 
-from keras.preprocessing.image import ImageDataGenerator
+#adam=tensorflow.keras.optimizers.Adam(learning_rate=0.001)
+model.compile(optimizer="Adam",loss='categorical_crossentropy',metrics=['accuracy',tensorflow.keras.metrics.Precision(),tensorflow.keras.metrics.Recall(),tensorflow.keras.metrics.AUC()])
+
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 train_datagen = ImageDataGenerator(
         rescale=1./255,
         shear_range=0.2,
@@ -68,37 +69,33 @@ train_datagen = ImageDataGenerator(
 test_datagen = ImageDataGenerator(rescale=1./255)
 
 training_set = train_datagen.flow_from_directory(
-        'maize_leaves/maize_leaves/train',
-        target_size=(128,128),
-        batch_size=16 ,
-        class_mode='binary')
+        '/content/drive/MyDrive/datasamenoofpics/train',
+        target_size=(150,150),
+        batch_size=32 ,
+        class_mode='categorical')
 
 val_set = test_datagen.flow_from_directory(
-        'maize_leaves/maize_leaves/test',
-        target_size=(128,128),
-        batch_size=16,
-        class_mode='binary')
+        '/content/drive/MyDrive/datasamenoofpics/test',
+        target_size=(150,150),
+        batch_size=32,
+        class_mode='categorical')
 
-model.fit_generator(
+history=model.fit_generator(
         training_set,
-        steps_per_epoch=326,
-        epochs=128,
+        steps_per_epoch=12,
+        epochs=10,
         validation_data=val_set,
-        validation_steps=39)
+        validation_steps=15)
 
+from google.colab import drive
+drive.mount('/content/drive')
 
-'''
-#Checking for individual images
+import matplotlib.pyplot as plt
 
-test_image = image.load_img('enter image name', target_size = (128, 128))
-
-test_image
-
-test_image = image.img_to_array(test_image)
-
-test_image = np.expand_dims(test_image, axis = 0)
-
-result = model.predict_classes(test_image)
-
-print(result)
-'''
+plt.subplot(211)  
+plt.plot(history.history['accuracy'])  
+plt.plot(history.history['val_accuracy'])  
+plt.title('model accuracy')  
+plt.ylabel('accuracy')  
+plt.xlabel('epoch')  
+plt.legend(['train', 'test'], loc='upper left')
